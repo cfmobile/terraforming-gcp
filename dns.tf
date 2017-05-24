d0037fb44e4b5338827e854d5539d188bb06d559
Ergin Babani <ebabani@pivotal.io>
2017-05-24 12:11:55 -0400

    [#146000343] Update dns name to dns suffix
    Signed-off-by: Jason Smith <jsmith@pivotal.io>

--- dns.tf
+++ dns.tf
@@ -1,5 +1,5 @@
 resource "google_dns_record_set" "ops-manager-dns" {
-  name = "pcf.${var.env_name}.${var.dns_name}"
+  name = "pcf.${var.env_name}.${var.dns_suffix}"
   type = "A"
   ttl  = 300
 
@@ -9,7 +9,7 @@ resource "google_dns_record_set" "ops-manager-dns" {
 }
 
 resource "google_dns_record_set" "optional-ops-manager-dns" {
-  name  = "pcf-optional.${var.env_name}.${var.dns_name}"
+  name  = "pcf-optional.${var.env_name}.${var.dns_suffix}"
   type  = "A"
   ttl   = 300
   count = "${min(length(split("", var.optional_opsman_image_url)),1)}"
@@ -20,7 +20,7 @@ resource "google_dns_record_set" "optional-ops-manager-dns" {
 }
 
 resource "google_dns_record_set" "wildcard-sys-dns" {
-  name = "*.sys.${var.env_name}.${var.dns_name}"
+  name = "*.sys.${var.env_name}.${var.dns_suffix}"
   type = "A"
   ttl  = 300
 
@@ -30,7 +30,7 @@ resource "google_dns_record_set" "wildcard-sys-dns" {
 }
 
 resource "google_dns_record_set" "doppler-sys-dns" {
-  name = "doppler.sys.${var.env_name}.${var.dns_name}"
+  name = "doppler.sys.${var.env_name}.${var.dns_suffix}"
   type = "A"
   ttl  = 300
 
@@ -40,7 +40,7 @@ resource "google_dns_record_set" "doppler-sys-dns" {
 }
 
 resource "google_dns_record_set" "loggregator-sys-dns" {
-  name = "loggregator.sys.${var.env_name}.${var.dns_name}"
+  name = "loggregator.sys.${var.env_name}.${var.dns_suffix}"
   type = "A"
   ttl  = 300
 
@@ -50,7 +50,7 @@ resource "google_dns_record_set" "loggregator-sys-dns" {
 }
 
 resource "google_dns_record_set" "wildcard-apps-dns" {
-  name = "*.apps.${var.env_name}.${var.dns_name}"
+  name = "*.apps.${var.env_name}.${var.dns_suffix}"
   type = "A"
   ttl  = 300
 
@@ -60,7 +60,7 @@ resource "google_dns_record_set" "wildcard-apps-dns" {
 }
 
 resource "google_dns_record_set" "wildcard-ws-dns" {
-  name = "*.ws.${var.env_name}.${var.dns_name}"
+  name = "*.ws.${var.env_name}.${var.dns_suffix}"
   type = "A"
   ttl  = 300
 
@@ -70,7 +70,7 @@ resource "google_dns_record_set" "wildcard-ws-dns" {
 }
 
 resource "google_dns_record_set" "app-ssh-dns" {
-  name = "ssh.sys.${var.env_name}.${var.dns_name}"
+  name = "ssh.sys.${var.env_name}.${var.dns_suffix}"
   type = "A"
   ttl  = 300
 
@@ -80,7 +80,7 @@ resource "google_dns_record_set" "app-ssh-dns" {
 }
 
 resource "google_dns_record_set" "tcp-dns" {
-  name = "tcp.${var.env_name}.${var.dns_name}"
+  name = "tcp.${var.env_name}.${var.dns_suffix}"
   type = "A"
   ttl  = 300
 
